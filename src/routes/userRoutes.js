35f8e3b0c649b0bfa61b6bb2cbfb337840cef05c
abdelghani bahri <bahri.ghanou@gmail.com>
2025-01-13 19:12:03 +0100

    improve register

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,6 +14,7 @@ const routes = (app) => {
             const savedUser = await user.save() //the function that saves the data in mongodb
             //later wew will add verification if the user exists
             if(savedUser) return res.json({success: true})
+            // if(savedUser) return res.redirect('http://localhost:3000/dashboard')
             return next(new Error ('Failed to save user for Unkown reasons'))
         } catch (error) {
             return next(error)
