fe8c5c596a68623a5af162c311d98309b06ff2ef
abdelghani bahri <bahri.ghanou@gmail.com>
2025-01-15 21:55:28 +0100

    import the controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,25 +1,8 @@
-import User from "../models/user.js"
+import { register } from "../controllers/userController"
 
 
 const routes = (app) => {
-    app.post('/register', async (req, res, next)=>{
-        try {
-            const userExist = await User.findOne({email: req.body.email})
-            if (userExist) return res.status(409).json({success: false, message: 'Email already used'})
-            const user = new User({
-                fullName : req.body.fullName,
-                email: req.body.email,
-                password: req.body.password
-            })
-            const savedUser = await user.save() //the function that saves the data in mongodb
-            //later wew will add verification if the user exists
-            if(savedUser) return res.json({success: true})
-            // if(savedUser) return res.redirect('http://localhost:3000/dashboard')
-            return next(new Error ('Failed to save user for Unkown reasons'))
-        } catch (error) {
-            return next(error)
-        }
-    })
+    app.post('/register', register)
 
 }
 
